293bd874642ec4ff6dc18a952864dff31665df44
Solid-Lizard <95529607+Solid-Lizard@users.noreply.github.com>
2023-06-12 11:55:13 +0200

    fix

--- src/main/java/com/bulelthell/game/bullethellbackend/config/WebSecurityConfig.java
+++ src/main/java/com/bulelthell/game/bullethellbackend/config/WebSecurityConfig.java
@@ -72,7 +72,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/api/auth/updateUser").authenticated()
 				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**").permitAll()
 				.antMatchers("/password/**").permitAll()
 				.antMatchers("/user/**").hasAnyRole("ROLE_ADMIN")
